bc8639d40274b5ac71503d8cf2a55ec65291795e
wissmedia <wissmedia@gmail.com>
2020-11-25 01:19:31 +0800

    menambah elevationscroll

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -2,12 +2,27 @@ import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+
+function ElevationScroll(props) {
+  const { children } = props;
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0
+  });
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
 export default function Header(props) {
   return (
-    <AppBar>
-      <Toolbar>
-        Paperon
-      </Toolbar>
-    </AppBar>
+    <ElevationScroll>
+      <AppBar position="fixed">
+        <Toolbar>
+          Paperon
+        </Toolbar>
+      </AppBar>
+    </ElevationScroll>
   )
 }
