4e4b55f58bf93e81b00653907e567ff452d0502d
wissmedia <wissmedia@gmail.com>
2021-08-11 22:41:45 +0800

    Update authRouter.js
    add more route to authrouter

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -5,7 +5,16 @@ let appTitle = 'Paperon'
 router.get('/signup', (req, res) => {
   res.render('auth/signup', { appTitle, navTitle: 'Daftar' })
 })
+router.post('/signup', (req, res) => {
+  res.render('auth/signup', { appTitle, navTitle: 'Daftar' })
+})
 router.get('/login', (req, res) => {
+  res.render('auth/login', { appTitle, navTitle: 'Masuk' })
+})
+router.post('/login', (req, res) => {
+  res.render('auth/login', { appTitle, navTitle: 'Masuk' })
+})
+router.get('/logout', (req, res) => {
   res.render('auth/login', { appTitle, navTitle: 'Masuk' })
 })
 
