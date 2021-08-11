033296d12910925a3ebf0af7689d8a07c9e08a25
wissmedia <wissmedia@gmail.com>
2021-08-11 14:03:14 +0800

    Add file to Public

---
+++ public/js/install-sw.js
@@ -0,0 +1,9 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .then((reg) => {
+      console.log('Service Worker Registered', reg)
+    })
+    .catch((err) => {
+      console.log('Service Worker Not Registered', err)
+    })
+}
