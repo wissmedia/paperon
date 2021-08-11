65143f3bb9833ee4818592c5535f1dfeff898f97
wissmedia <wissmedia@gmail.com>
2021-08-11 21:14:17 +0800

    Update config.js
    Change default app port

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const config = {
   app: {
     host: process.env.APP_HOST || "localhost",
-    port: parseInt(process.env.APP_PORT) || 1234,
+    port: parseInt(process.env.APP_PORT) || 2121,
   },
   db: {},
 }
