211c70ccc94bf9723a5087ed557913b8326a26c6
wissmedia <wissmedia@gmail.com>
2021-08-11 22:25:40 +0800

    Update app.js
    Create error handler and route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const morgan = require('morgan')
+const createError = require('http-errors');
 
 const config = require('./config')
 
@@ -54,3 +55,19 @@ app.get('/admin/qbank/add', (req, res) => {
 app.get('/admin/qbank/detail', (req, res) => {
   res.render('qbank/qbank-detail', { appTitle, navTitle: 'Detail Pertanyaan' })
 })
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error', { appTitle, navTitle: '404' });
+});
