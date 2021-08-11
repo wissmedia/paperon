dab256d886d81c7cd044b27764f8a43285432aee
wissmedia <wissmedia@gmail.com>
2021-08-11 13:04:42 +0800

    Update app.js
    Add simple code to run the app

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const morgan = require('morgan')
+
+const config = require('./config')
+
+const app = express()
+const host = config.app.host
+const port = config.app.port
+
+app.listen(port, host, () => {
+  console.log(`App Listening at http://${host}:${port}`)
+})
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
