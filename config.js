0a14f371bbd4c7b11e79bc2c21c532cb804f655d
wissmedia <wissmedia@gmail.com>
2021-08-12 07:56:18 +0800

    Update config.js
    Add db config

--- config.js
+++ config.js
@@ -5,7 +5,14 @@ const config = {
     host: process.env.APP_HOST || "localhost",
     port: parseInt(process.env.APP_PORT) || 2121,
   },
-  db: {},
+  db: {
+    host: process.env.DB_HOST || "localhost",
+    port: parseInt(process.env.DB_PORT || 27017),
+    user: process.env.DB_USERNAME || "admin",
+    pass: process.env.DB_PASSWORD || "password",
+    data: process.env.DB_DATABASE || "db_test",
+    string: process.env.DB_STRING
+  },
 }
 
 module.exports = config
